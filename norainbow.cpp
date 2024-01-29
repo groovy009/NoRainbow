41f1dc7880d92dc88447396663ea3012eea6082a
Tyler Herrman <91999666+groovy009@users.noreply.github.com>
2024-01-28 20:42:27 -0500

    Add files via upload

---
+++ norainbow.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+#define SECTION_SIZE 4
+#define PATH_SIZE 5
+using namespace std;
+
+bool proper = true;
+
+int checkLegal(int section[SECTION_SIZE]){
+
+
+}
+
+int main(){
+	int[PATH_SIZE] path;
+
+	
+
+
+
+
+
+	return 0;
+}
